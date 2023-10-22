8ac7ba42b4f1b0c9fcc8c336730e04c808a7adea
Gamal Samadov <gamalsamadov@gmail.com>
2023-10-22 10:11:08 +0300

    Webhook fixing

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -1,36 +1,55 @@
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 from hsoub.settings import STRIPE_ENDPOINT_SECRET
 from django.http import HttpResponse
 from checkout.models import Transaction
 from academy.models import Order, Course
-import stripe
+import stripe, json
 
 
+@require_POST
 @csrf_exempt
 def stripe_webhook(request):
-  payload = request.body
-  sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
 
-  try:
-      event = stripe.Webhook.construct_event(
-          payload, sig_header, STRIPE_ENDPOINT_SECRET
-      )
-  except ValueError as e:
-      return HttpResponse(status=400)
-  except stripe.error.SignatureVerificationError as e:
-      return HttpResponse(status=400)
+    try:
+        # event = stripe.Webhook.construct_event(
+        #     payload, sig_header, STRIPE_ENDPOINT_SECRET
+        # )
+        event = stripe.WebhookEndpoint.create(
+          url='https://hsoub-exam-production.up.railway.app/en/checkout/stripe/webhook',
+          enabled_events=[
+            'payment_intent.succeeded',
+          ],
+        )
 
-  # Handle the event
-  if event.type == 'payment_intent.succeeded':
-    payment_intent = event.data.object
-    transaction_id = payment_intent.metadata.transaction
-    make_order(transaction_id)
-    print(transaction_id)
+        # event = stripe.Event.construct_from(
+        #     json.loads(payload), stripe.api_key
+        #   )
+    except ValueError as e:
+        print('Error parsing payload: {}'.format(str(e)))
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        print('Error verifying webhook signature: {}'.format(str(e)))
+        return HttpResponse(status=400)
 
-  else:
-    print('Unhandled event type {}'.format(event['type']))
+    # Handle the event
+    if event.type == 'payment_intent.succeeded':
+      # print('PaymentIntent was successful!')
+      payment_intent = event.data.object
+      transaction_id = payment_intent.metadata.transaction
+      make_order(transaction_id)
+    
+    elif event.type == 'payment_method.attached':
+      print('PaymentMethod was attached to a Customer!')
 
-  return HttpResponse(status=200)
+    else:
+      print('Unhandled event type {}'.format(event['type']))
+
+    return HttpResponse(status=200)
+
 
 
 def make_order(transaction_id):
